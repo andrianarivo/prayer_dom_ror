369a519f3c6ed2ae7f0f77059217a354ed4c92d9
David Andrianarivo <andrianarivo.david@gmail.com>
2024-03-24 22:07:00 +0300

    refactor

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Home', type: :request do
+  subject do
+    FactoryBot.create(:user)
+  end
+
+  before(:each) do
+    sign_in subject
+  end
+
   describe 'GET /index' do
     it 'returns http success' do
       get '/'
       # it is :found (302) because it redirects to /users/sign_in
-      expect(response).to have_http_status(:found)
+      expect(response).to have_http_status(:success)
     end
   end
 end

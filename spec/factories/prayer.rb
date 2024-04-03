57f7ddc94412368088beffe08661fd1c37f0282a
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-03 20:09:25 +0300

    Add priority level to prayer factory

--- spec/factories/prayer.rb
+++ spec/factories/prayer.rb
@@ -8,5 +8,6 @@ FactoryBot.define do
     datetime_to_pray { Faker::Date.backward(days: 30) }
     location { Faker::Address.city }
     summary { Faker::Lorem.sentence(word_count: rand(2..8)) }
+    priority { PriorityLevels::LEVELS.sample }
   end
 end

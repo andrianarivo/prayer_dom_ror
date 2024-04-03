9d10766af3118bf9098424eda36ffbc71864d983
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-03 20:08:43 +0300

    Add PriorityLevels module

---
+++ app/models/concerns/priority_levels.rb
@@ -0,0 +1,7 @@
+module PriorityLevels
+  VERY_HIGH = 'very high'.freeze
+  HIGH = 'high'.freeze
+  LOW = 'low'.freeze
+  VERY_LOW = 'very low'.freeze
+  LEVELS = [VERY_HIGH, HIGH, LOW, VERY_LOW].freeze
+end

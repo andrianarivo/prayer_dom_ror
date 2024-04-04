2dcb834990bc2cd33daf0292b4363ba0d1f56082
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-04 20:06:59 +0300

    Add role constants to the application

---
+++ app/models/concerns/role_constants.rb
@@ -0,0 +1,7 @@
+module RoleConstants
+  ADMIN = 'admin'.freeze
+  USER = 'user'.freeze
+  MODERATOR = 'moderator'.freeze
+  GUEST = 'guest'.freeze
+  ROLES = [ADMIN, USER, MODERATOR, GUEST].freeze
+end

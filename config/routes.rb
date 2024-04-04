a6d15d6f84c9ea2de9e7f170d70940b81d75903d
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-04 20:07:09 +0300

    Add routes for roles

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :types
   resources :topics
   resources :tags
+  resources :roles
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

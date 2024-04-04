170c8092c4697eafd8dea9f2ab17b0d4f8272e2a
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-04 20:09:39 +0300

    Add spec for new role form

---
+++ spec/views/roles/new.html.tailwindcss_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'roles/new', type: :view do
+  before(:each) do
+    assign(:role, Role.new)
+  end
+
+  it 'renders new role form' do
+    render
+
+    assert_select 'form[action=?][method=?]', roles_path, 'post' do
+      assert_select 'input[name=?]', 'role[title]'
+    end
+  end
+end

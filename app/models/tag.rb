6c6852d0294c8e3c0a768f2e83933522be38c9ac
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-09 20:36:15 +0300

    Update tag.rb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,8 @@
 class Tag < ApplicationRecord
-  has_many :prayers, dependent: :destroy
   has_many :topics, dependent: :destroy
   has_many :types, dependent: :destroy
+
+  has_and_belongs_to_many :prayers
 
   validates :label, presence: true
 end

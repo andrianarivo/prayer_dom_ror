acf08c678902e48c952e54c120709f53e6ce71ad
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-09 20:36:31 +0300

    Update prayer.rb

--- spec/factories/prayer.rb
+++ spec/factories/prayer.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
     association :status
     association :type
     association :user
-    association :tag
     datetime_to_pray { Faker::Date.backward(days: 30) }
     location { Faker::Address.city }
     summary { Faker::Lorem.sentence(word_count: rand(2..8)) }
     priority { PriorityLevels::LEVELS.sample }
+    tags { FactoryBot.create_list(:tag, rand(1..3)) }
   end
 end

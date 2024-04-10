82888db502aeec64c6cb7c1f8e9ddf134227a9a0
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-10 21:58:16 +0300

    Update index.html.tailwindcss_spec.rb

--- spec/views/prayers/index.html.tailwindcss_spec.rb
+++ spec/views/prayers/index.html.tailwindcss_spec.rb
@@ -1,19 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'prayers/index', type: :view do
+  let(:prayers) { FactoryBot.create_list(:prayer, 2) }
+
   before(:each) do
-    assign(:prayers, FactoryBot.create_list(:prayer, 2))
+    assign(:prayers, prayers)
+    render
   end
 
   it 'renders a list of prayers' do
-    render
-    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-    assert_select cell_selector, text: Regexp.new('Description'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('Status'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('Type'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('User'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('Location'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('Summary'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('Priority'.to_s), count: 2
+    assert_select 'div#prayers' do
+      prayers.each do |prayer|
+        assert_select 'div.relative' do
+          assert_select '.max-w-sm' do
+            assert_select '.font-medium', text: prayer.summary
+            assert_select '.text-gray-500', text: prayer.description
+            assert_select '.text-gray-500', text: "#{prayer.notes_count} notes"
+            assert_select '.text-gray-500', text: "#{prayer.answers_count} answers"
+            assert_select '.text-base', text: prayer.user.username.capitalize
+            assert_select '.text-lg', text: formatted_date(prayer.datetime_to_pray)
+          end
+        end
+      end
+    end
   end
 end

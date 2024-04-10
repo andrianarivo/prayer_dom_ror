bdac8cb7bd7385970c392356d1cf0715f6b4e49d
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-10 21:58:17 +0300

    Update show.html.tailwindcss_spec.rb

--- spec/views/prayers/show.html.tailwindcss_spec.rb
+++ spec/views/prayers/show.html.tailwindcss_spec.rb
@@ -1,18 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'prayers/show', type: :view do
+  let(:prayer) { FactoryBot.create(:prayer) }
+
   before(:each) do
-    assign(:prayer, FactoryBot.create(:prayer))
+    assign(:prayer, prayer)
+    render
   end
 
-  it 'renders attributes in <p>' do
-    render
-    expect(rendered).to match(/Description/)
-    expect(rendered).to match(/Type/)
-    expect(rendered).to match(/User/)
-    expect(rendered).to match(/Status/)
-    expect(rendered).to match(/Location/)
-    expect(rendered).to match(/Summary/)
-    expect(rendered).to match(/Priority/)
+  it 'renders the prayer details' do
+    expect(rendered).to have_selector("div#prayer_#{prayer.id}.m-6")
+
+    # Check for tags
+    prayer.tags.each do |tag|
+      expect(rendered).to have_selector("span[style='background-color: #{tag.bg_color}; color: #{tag.color}']",
+                                        text: tag.label.capitalize)
+    end
+
+    # Check for prayer summary and description
+    expect(rendered).to have_selector('div', text: prayer.summary)
+    expect(rendered).to have_selector('p', text: prayer.description)
+
+    # Check for notes count and answers count
+    expect(rendered).to have_selector('p', text: "#{prayer.notes_count} notes")
+    expect(rendered).to have_selector('p', text: "#{prayer.answers_count} answers")
+
+    # Check for user details
+    expect(rendered).to have_selector('div#avatar', text: prayer.user.username[0].upcase)
+    expect(rendered).to have_selector('p', text: prayer.user.username.capitalize)
+
+    # Check for formatted date
+    expect(rendered).to have_selector('p', text: formatted_date(prayer.datetime_to_pray))
   end
 end

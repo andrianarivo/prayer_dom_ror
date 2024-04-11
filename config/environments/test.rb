f725f8bb97628943343dcd32e66f1dfca0526a8b
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-11 21:51:55 +0300

    Update test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,4 +61,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  Rails.application.routes.default_url_options = { locale: I18n.default_locale }
 end

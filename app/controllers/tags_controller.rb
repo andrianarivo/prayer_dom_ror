791064f48486c86c91b9c586c91109cb871d79fe
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-16 18:41:32 +0300

    Update tags_controller.rb

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -65,6 +65,6 @@ class TagsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def tag_params
-    params.require(:tag).permit(:label)
+    params.require(:tag).permit(:label, :bg_color, :color)
   end
 end

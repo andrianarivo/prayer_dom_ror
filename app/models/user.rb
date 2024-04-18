18045ae48807dc907fd22774568624a5c85cf5d5
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-18 20:09:06 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   belongs_to :role
 
   validates :email, :username, :role, presence: true

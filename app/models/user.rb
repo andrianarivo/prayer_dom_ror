4bb77e00c9897846d85bf3b9fd1cd368185794e6
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-23 16:15:48 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_one_attached :avatar
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [24, 24]
+  end
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable

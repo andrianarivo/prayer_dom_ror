85b77ee6d7dfeb38bbe833a17ee03a692ce5c039
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-23 16:15:56 +0300

    Update tailwind.config.js

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -28,5 +28,6 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
+    require('daisyui')
   ]
 }

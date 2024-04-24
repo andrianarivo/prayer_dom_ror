5f622ac19c1e73b0ec1731b97e140417f6e6d21d
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-24 19:42:19 +0300

    Update show.html.tailwindcss_spec.rb

--- spec/views/notes/show.html.tailwindcss_spec.rb
+++ spec/views/notes/show.html.tailwindcss_spec.rb
@@ -7,8 +7,7 @@ RSpec.describe 'notes/show', type: :view do
 
   it 'renders attributes in <p>' do
     render
-    expect(rendered).to match(//)
+    expect(rendered).to match(/Note id/)
     expect(rendered).to match(/Title/)
-    expect(rendered).to match(/Content/)
   end
 end

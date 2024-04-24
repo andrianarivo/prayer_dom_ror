3039fb398362b9990199d2c7c17c6ccde3cb7123
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-24 19:42:16 +0300

    Update index.html.tailwindcss_spec.rb

--- spec/views/notes/index.html.tailwindcss_spec.rb
+++ spec/views/notes/index.html.tailwindcss_spec.rb
@@ -8,8 +8,7 @@ RSpec.describe 'notes/index', type: :view do
   it 'renders a list of notes' do
     render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-    assert_select cell_selector, text: Regexp.new('Prayer'.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('Note id'.to_s), count: 2
     assert_select cell_selector, text: Regexp.new('Title'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('Content'.to_s), count: 2
   end
 end

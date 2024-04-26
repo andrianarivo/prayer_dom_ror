c49d352db7244aa81183c1e2e903c5ba4047f7e7
David Andrianarivo <andrianarivo.david@gmail.com>
2024-04-26 21:16:50 +0300

    Refactor application_helper.rb and user.rb, and update note.html.erb and _avatar.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,17 +8,11 @@ module ApplicationHelper
     formatted_date
   end
 
-  def darken_color(hex_color, amount)
-    # Darken the color by reducing each RGB component
-    r = hex_color[0..1].to_i(16) * (1 - amount)
-    g = hex_color[2..3].to_i(16) * (1 - amount)
-    b = hex_color[4..5].to_i(16) * (1 - amount)
-
-    # Convert RGB components back to hex and return the result
-    format('#%<red>02x%<green>02x%<blue>02x', red: r, green: g, blue: b)
-  end
-
   def avatar_text(name)
     name[0].upcase
+  end
+
+  def get_or_default(size, default)
+    size || default
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [24, 24]
+    attachable.variant :small, resize_to_limit: [48, 48]
   end
 
   # Include default devise modules. Others available are:

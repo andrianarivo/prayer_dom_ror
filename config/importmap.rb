1b0bdf9e3723adb36431e25cc37955b71c6bd6e3
David Andrianarivo <andrianarivo.david@gmail.com>
2024-05-10 20:19:15 +0300

    Update importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "luxon" # @3.4.4

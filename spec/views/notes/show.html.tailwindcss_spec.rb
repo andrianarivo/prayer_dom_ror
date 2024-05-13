2fc69b4f9dcfdcba03835804d90057386b9032ec
David Andrianarivo <andrianarivo.david@gmail.com>
2024-05-13 19:12:17 +0300

    Update show.html.tailwindcss_spec.rb

--- spec/views/notes/show.html.tailwindcss_spec.rb
+++ spec/views/notes/show.html.tailwindcss_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'notes/show', type: :view do
+  let(:note) { FactoryBot.create(:note) }
+
   before(:each) do
-    assign(:note, FactoryBot.create(:note))
+    assign(:note, note)
+    render
   end
 
-  it 'renders attributes in <p>' do
-    render
-    expect(rendered).to match(/Note id/)
-    expect(rendered).to match(/Title/)
+  it 'renders the note' do
+    expect(rendered).to have_selector("div#note_#{note.id}")
+
+    # Check for note title and content
+    expect(rendered).to have_selector('h3', text: note.title)
+    expect(rendered).to have_selector('p', text: note.content)
+
+    # Check for prayer time
+    expect(rendered).to have_selector("time[data-controller='timeago'][datetime='#{note.created_at}']")
   end
 end

7f32e1c6be75d4fe189504bc66fb8e9017bc51e5
David Andrianarivo <andrianarivo.david@gmail.com>
2024-05-13 19:12:14 +0300

    Update index.html.tailwindcss_spec.rb

--- spec/views/notes/index.html.tailwindcss_spec.rb
+++ spec/views/notes/index.html.tailwindcss_spec.rb
@@ -1,14 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'notes/index', type: :view do
+  let(:notes) { FactoryBot.create_list(:note, 2) }
+
   before(:each) do
-    assign(:notes, FactoryBot.create_list(:note, 2))
+    assign(:notes, notes)
+    render
   end
 
   it 'renders a list of notes' do
-    render
-    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-    assert_select cell_selector, text: Regexp.new('Note id'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('Title'.to_s), count: 2
+    assert_select 'div#notes' do
+      notes.each do |note|
+        assert_select 'div.chat' do
+          assert_select '.chat-image>.avatar'
+          assert_select '.chat-bubble' do
+            assert_select 'h3', text: note.title
+            assert_select 'p', text: note.content
+          end
+          assert_select '.chat-footer' do
+            assert_select "time[data-controller='timeago'][datetime='#{note.created_at}']"
+          end
+        end
+      end
+    end
   end
 end
